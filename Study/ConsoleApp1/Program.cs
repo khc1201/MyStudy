11be79a8ef9d49919e0979942d4113a262df0656
khc1201 <41064972+khc1201@users.noreply.github.com>
2022-07-30 19:26:04 +0900

    이걸로저장
    저장

--- Study/ConsoleApp1/Program.cs
+++ Study/ConsoleApp1/Program.cs
@@ -1,4 +1,10 @@
-﻿public class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program
 {
     public static void Main()
     {
@@ -9,4 +15,254 @@
 public interface IStudy
 {
     public void DoAction();
+}
+
+
+public class MTree
+{
+    private MTreeNode m_root;
+    private int m_count = 0;
+    public int Count { get { return m_count; } }
+
+    public void Append(int value)
+    {
+        if (m_root == null)
+        {
+            m_root = new MTreeNode(this, null, value);
+            m_count++;
+            return;
+        }
+
+        MTreeNode node = m_root;
+        while (node != null)
+        {
+            if (node.m_value > value)
+            {
+                if (node.m_child_left == null)
+                {
+                    node.m_child_left = new MTreeNode(this, node, value);
+                    m_count++;
+                    break;
+                }
+                else
+                {
+                    node = node.m_child_left;
+                }
+            }
+            else if (node.m_value < value)
+            {
+                if (node.m_child_right == null)
+                {
+                    node.m_child_right = new MTreeNode(this, node, value);
+                    m_count++;
+                    break;
+                }
+                else
+                {
+                    node = node.m_child_right;
+                }
+            }
+            else
+            {
+                throw new Exception($"[System : Error!!!] value({value})의 값은 중복입니다. 유니크한 값이 보장되어야 합니다.");
+            }
+        }
+    }
+    public void Append(int value, char charValue)
+    {
+        if (m_root == null)
+        {
+            m_root = new MTreeNode(this, null, value, charValue);
+            m_count++;
+            return;
+        }
+
+        MTreeNode node = m_root;
+        while (node != null)
+        {
+            if (node.m_value > value)
+            {
+                if (node.m_child_left == null)
+                {
+                    node.m_child_left = new MTreeNode(this, node, value, charValue);
+                    m_count++;
+                    break;
+                }
+                else
+                {
+                    node = node.m_child_left;
+                }
+            }
+            else if (node.m_value < value)
+            {
+                if (node.m_child_right == null)
+                {
+                    node.m_child_right = new MTreeNode(this, node, value, charValue);
+                    m_count++;
+                    break;
+                }
+                else
+                {
+                    node = node.m_child_right;
+                }
+            }
+            else
+            {
+                throw new Exception($"[System : Error!!!] value({value})의 값은 중복입니다. 유니크한 값이 보장되어야 합니다.");
+            }
+        }
+    }
+
+    public MTreeNode GetTreeNodeByValue(int value)
+    {
+        if (m_root == null)
+        {
+            throw new NullReferenceException($"[System : Error!!!] root 가 존재하지 않습니다.");
+        }
+
+        MTreeNode resultNode = CompareNodeValue(m_root, value);
+        if (resultNode == null)
+        {
+            throw new NullReferenceException($"[System : Error!!!] findValue({value})가 tree 내에 존재하지 않습니다.");
+        }
+        return resultNode;
+    }
+    private MTreeNode CompareNodeValue(MTreeNode node, int findValue)
+    {
+        if (node.m_value == findValue)
+        {
+            return node;
+        }
+        else if (node.m_value > findValue)
+        {
+            if (node.m_child_left != null) return CompareNodeValue(node.m_child_left, findValue);
+        }
+        else if (node.m_value < findValue)
+        {
+            if (node.m_child_right != null) return CompareNodeValue(node.m_child_right, findValue);
+        }
+        return null;
+    }
+
+    StringBuilder m_sb = new StringBuilder();
+    public enum ShowType {PreOrder, InOrder, PostOrder}
+    public void ShowTree(ShowType type, bool isShowChar = false)
+    {
+        m_sb.Clear();
+        m_sb.Append('[');
+        string typeString = "";
+        switch (type)
+        {
+            case ShowType.PreOrder:
+                {
+                    typeString = "PreOrder";
+
+                    ShowNode_Recursive_PreOrder(m_root, isShowChar);
+                    break;
+                }
+            case ShowType.InOrder:
+                {
+                    typeString = "InOrder";
+                    ShowNode_Recursive_InOrder(m_root, isShowChar);
+
+                    break;
+                }
+            case ShowType.PostOrder:
+                {
+                    typeString = "PostOrder";
+                    ShowNode_Recursive_PostOrder(m_root, isShowChar);
+
+                    break;
+                }
+        }
+        //PreOrder
+        
+        
+        m_sb.Append(']');
+        Console.WriteLine($"[Test] ShowTree() - Type({typeString}) : " + m_sb.ToString());
+    }
+    public void Clear()
+    {
+        m_root = null;
+        m_count = 0;
+    }
+    private void ShowNode_Recursive_PreOrder(MTreeNode node, bool isShowChar)
+    {
+
+        if (isShowChar)
+        {
+            m_sb.Append((char)node.m_charValue);
+        }
+        else
+        {
+            m_sb.Append(node.m_value + ", ");
+        }
+
+        if (node.m_child_left != null)
+        {
+            ShowNode_Recursive_PreOrder(node.m_child_left, isShowChar);
+        }
+        if (node.m_child_right != null)
+        {
+            ShowNode_Recursive_PreOrder(node.m_child_right, isShowChar);
+        }
+    }
+    private void ShowNode_Recursive_InOrder(MTreeNode node, bool isShowChar)
+    {
+
+        if (node.m_child_left != null)
+        {
+            ShowNode_Recursive_InOrder(node.m_child_left, isShowChar);
+        }
+        if (isShowChar)
+        {
+            m_sb.Append((char)node.m_charValue);
+        }
+        else
+        {
+            m_sb.Append(node.m_value + ", ");
+        }
+        if (node.m_child_right != null)
+        {
+            ShowNode_Recursive_InOrder(node.m_child_right, isShowChar);
+        }
+    }
+    private void ShowNode_Recursive_PostOrder(MTreeNode node, bool isShowChar)
+    {
+        if (node.m_child_left != null)
+        {
+            ShowNode_Recursive_PostOrder(node.m_child_left, isShowChar);
+        }
+        if (node.m_child_right != null)
+        {
+            ShowNode_Recursive_PostOrder(node.m_child_right, isShowChar);
+        }
+        if (isShowChar)
+        {
+            m_sb.Append((char)node.m_charValue);
+        }
+        else
+        {
+            m_sb.Append(node.m_value + ", ");
+        }
+    }
+
+}
+public class MTreeNode
+{
+    public MTree m_tree;
+    public MTreeNode m_parent;
+    public MTreeNode m_child_left;
+    public MTreeNode m_child_right;
+
+    public int m_value;
+    public int m_charValue;
+
+    public MTreeNode(MTree tree, MTreeNode parent, int value, char charValue = ' ')
+    {
+        m_tree = tree;
+        m_parent = parent;
+        m_value = value;
+        m_charValue = charValue;
+    }
 }
--- Study/ConsoleApp1/Study0730.cs
+++ Study/ConsoleApp1/Study0730.cs
@@ -25,11 +25,36 @@ namespace HomeWork0730_1
      */
     public class Homework : IStudy
     {
+        /*
+        *         E              
+        *      /     \
+        *     X       N
+        *    / \
+        *   A   U
+        *  / \
+        * M   F
+        */
         public void DoAction()
         {
+            char[] charArray = { 'E', 'X', 'N', 'A', 'U', 'M', 'F' };
+            int[] indexArray = {5,3,6,1,4,0,2};
+            
+
+            MTree myTree = new MTree();
+            for(int i =0; i < charArray.Length; i++)
+            {
+                myTree.Append(indexArray[i], charArray[i]);
+            }
+
+            myTree.ShowTree(MTree.ShowType.PreOrder, true);
+            myTree.ShowTree(MTree.ShowType.InOrder, true);
+            myTree.ShowTree(MTree.ShowType.PostOrder, true);
 
         }
+       
+
     }
+
 }
 namespace HomeWork0730_2
 {
@@ -38,8 +63,102 @@ namespace HomeWork0730_2
      */
     public class Homework : IStudy
     {
+        int[] array;
+
         public void DoAction()
         {
+            while (true)
+            {
+                Console.Write("[System : Input] 값 입력 = ");
+                int input = int.Parse(Console.ReadLine());
+                if (input == 0) 
+                {
+                    Console.WriteLine("[System : Error] input 값이 정상적이지 않습니다. input = " + input);
+                    continue; 
+                }
+
+                array = new int[input+1]; //초기화. 값이 가장 마지막이므로 +1을 함 (0은 사용하지 않을 예정?)
+
+                Console.WriteLine("[System : Result] 최솟값 = " + Recursive(input));
+                Console.WriteLine("[System : Appendix] 최소값 배열 = " + GetArrayString()); 
+                Console.WriteLine("---------------------------------------------");
+            }
+        }
+        private string GetArrayString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append('[');
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                sb.Append(array[i]);
+                if (i != array.Length - 1) sb.Append(", ");
+            }
+
+            sb.Append(']');
+            return sb.ToString();
+        }
+        private int Recursive(int value)
+        {
+            if(value == 1)
+            { 
+                return 0; //0을 반환하여 아무것도 늘리지 않음. 더이상 순회하지 않고 종료함.
+            }
+
+            if (array[value] > 0)
+            {
+                //이 경우에는 이미 값이 존재하는 경우이므로 바로 자신의 값을 반환.
+                return array[value];
+            }
+
+            array[value] = Recursive(value - 1) + 1; //일단 1을 뺀값에서 + 1(횟수) 함.
+            if(value%3 == 0)
+            {
+                int tempCount = Recursive(value / 3) + 1; //3으로 나눈 값 중 +1(횟수) 함.
+                if(array[value] > tempCount)
+                {
+                    //3으로 나눈 값과 다른 값들이 각각 최솟값을 갖는지 확인
+                    //for test
+                    Console.WriteLine($" ㄴ[Test - value % 3] 현재 등록된 array[{value}]의 최소값은 ({array[value]}) 였는데, %3+1 했을 때 이 값이 더 작으므로 ({tempCount})를 최소값으로 변경!");
+                    array[value] = tempCount;
+                } 
+            }
+            if(value%2 == 0)
+            {
+                int tempCount = Recursive(value / 2) + 1;
+                if (array[value] > tempCount)
+                {
+                    //for test
+                    Console.WriteLine($" ㄴ[Test - value % 2] 현재 등록된 array[{value}]의 최소값은 ({array[value]}) 였는데, %2+1 했을 때 이 값이 더 작으므로 ({tempCount})를 최소값으로 변경!");
+
+                    //3으로 나눈 값과 다른 값들이 각각 최솟값을 갖는지 확인
+                    array[value] = tempCount;
+                }
+            }
+
+            return array[value];
+        }
+
+
+    }
+}
+
+namespace MyStudy0730
+{
+    //트리 다시 만들어보기
+    public class Homework : IStudy
+    { 
+
+        public void DoAction()
+        {
+            int[] input = { 1000,100,10000,10,101,1001,10001,5,50 };
+
+            MTree myTree = new MTree();
+            
+            foreach(int value in input)
+            {
+                myTree.Append(value);
+            }
 
         }
     }
